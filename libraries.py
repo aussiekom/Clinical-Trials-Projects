ae6ff802d6cd86c4764bc345a6a2b0c350c905c2
Evgeniia Komarova <102028836+aussiekom@users.noreply.github.com>
2023-09-01 12:50:00 +0200

    Add files via upload

---
+++ libraries.py
@@ -0,0 +1,52 @@
+# import the necessary libraries
+import numpy as np 
+import pandas as pd 
+import os
+from xml.etree import ElementTree
+
+# text processing libraries
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+
+# Visualisation libraries
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+sns.set()
+import pycountry
+import plotly.express as px
+from plotly.offline import init_notebook_mode, iplot 
+import plotly.graph_objs as go
+import plotly.offline as py
+from plotly.offline import download_plotlyjs,init_notebook_mode,plot,iplot
+!pip install chart_studio
+import chart_studio.plotly as py
+import cufflinks
+cufflinks.go_offline()
+cufflinks.set_config_file(world_readable=True, theme='pearl')
+#py.init_notebook_mode(connected=True)
+
+#Geographical Plotting
+import folium
+from folium import Choropleth, Circle, Marker
+from folium import plugins
+from folium.plugins import HeatMap, MarkerCluster
+
+#Racing Bar Chart
+!pip install bar_chart_race
+import bar_chart_race as bcr
+from IPython.display import HTML
+
+# Increase the default plot size and set the color scheme
+plt.rcParams['figure.figsize'] = 8, 5
+plt.style.use("fivethirtyeight")# for pretty graphs
+
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# Disable warnings 
+import warnings
+warnings.filterwarnings('ignore')
